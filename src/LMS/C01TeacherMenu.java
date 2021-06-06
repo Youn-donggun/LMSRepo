45fd474d336841796aaed97d86b6a95a73a76abe
정윤호 <@DESKTOP-9TDU6U3>
2021-06-06 23:24:34 +0900

    B ver 0.3

--- src/LMS/C01TeacherMenu.java
+++ src/LMS/C01TeacherMenu.java
@@ -24,7 +24,7 @@ public class C01TeacherMenu {
 	C01Teacher CurIdx;	// 현재 로그인한 교수 Idx 저장
 	
 	Scanner sc = new Scanner(System.in);
-	int no = 0;
+	String str;
 	void run() {
 		while(true)
 		{
@@ -38,31 +38,30 @@ public class C01TeacherMenu {
 		System.out.println("7.종료");
 		System.out.println("------------ 교수메뉴 -------------");
 		System.out.print("입력 :");
-		no = sc.nextInt();
-		switch(no)
+		str = sc.next();
+		switch(str)
 		{
-		case 1:
+		case "1":
 			//계정 가입
 			//C01Teacher객체 만들고 각멤버에 값 넣어서 
 			//(교수ID , 교수이름 , 교수과목, 이메일주소)
 			//ArrayList에 저장
-			System.out.print("ID : ");
+			System.out.print("교수 ID : ");
 			tch.TID = sc.next();
-			System.out.print("Name : ");
+			System.out.print("교수 이름 : ");
 			tch.Name = sc.next();
-			System.out.print("Subject : ");
+			System.out.print("교수 과목 : ");
 			tch.Subject = sc.next();
-			System.out.print("Email : ");
+			System.out.print("이메일 주소 : ");
 			tch.Email = sc.next();
 			Tchlist.add(tch);
-			System.out.println(Tchlist);
 			System.out.println("계정 등록 완료!");
 			break;
-		case 2:
+		case "2":
 			//로그인
 			//ArrayList에 해당 TId가 있는 idx를 CurIdx에 저장
 			sc.nextLine();
-			System.out.print("ID : ");
+			System.out.print("교수 ID : ");
 		    String id = sc.nextLine();
 		    
 		    for(C01Teacher teacher : Tchlist) {
@@ -74,38 +73,51 @@ public class C01TeacherMenu {
 		        } 
 		    }
 			break;
-		case 3:
+		case "3":
 			//로그아웃
 			//CurIdx 에 9999값 넣음 
 			CurIdx = null;
 			System.out.println("로그아웃합니다");
 			return;
-		case 4:
+		case "4":
 			//출석부
 			//S안에 있는 내용 참고해서 idx에 있는 교수과목과 동일한
 			//과목을 듣는 학생리스트를 출력 
 			break;
-		case 5:
+		case "5":
 			//정보변경
 			//해당 idx에 있는 정보 변경 
 			System.out.println("교수 정보를 수정합니다.(ID제외)");
-			System.out.print("Name : ");
+			System.out.print("교수 이름 : ");
 			CurIdx.Name = sc.next();
-			System.out.print("Subject : ");
+			System.out.print("교수 과목 : ");
 			CurIdx.Subject = sc.next();
-			System.out.print("Email : ");
+			System.out.print("이메일 주소 : ");
 			CurIdx.Email = sc.next();
 			System.out.println("수정 완료!");
 			break;
-		case 6:
+		case "6":
 			//계정삭제
 			//해당 idx에 있는 정보 삭제
-			System.out.print("삭제할 ID : ");
-			tch.TID = sc.next();
-			Tchlist.remove(tch);
-			System.out.println("계정 삭제 완료!");
+			System.out.print("ID를 입력해주세요 : ");
+			String delID = sc.next();
+			try {
+				if(CurIdx.TID.equals(delID)) {
+					System.out.println("정말로 삭제 하시겠습니까?(\"네\"라고 입력)");
+					String confirm = sc.next();
+					if(confirm.equals("네")) {
+						Tchlist.remove(CurIdx);
+						System.out.println("계정 삭제 완료");
+						CurIdx = null;
+					}
+		        } else {
+			        System.out.println("ID가 틀립니다.");
+		        }
+			} catch (Exception e) {
+		        System.out.println("ID가 틀립니다.");
+			}
 			break;
-		case 7:
+		case "7":
 			//종료
 			System.out.println("종료합니다");
 			return;
