11897dc2a504f22e7a79f28278113fcf68dc4e22
y9507 <y9507@DESKTOP-F0D7E3I>
2021-06-06 22:32:23 +0900

    Team A ver0.1

--- src/LMS/C01StudentMenu.java
+++ src/LMS/C01StudentMenu.java
@@ -13,14 +13,13 @@ class C01Student
 	String stdPhone;
 	String Subject[];	//수강신청과목 저장 
 }
-
 public class C01StudentMenu {
 	
 	ArrayList<C01Student> Stdlist = new ArrayList();
-	C01Student CurIdx;	// 현재 로그인한 학생 idx 저장  
+	C01Student CurIdx;	// 현재 로그인한 학생 객체 저장  
 	
 	Scanner sc = new Scanner(System.in);
-	int no=0;
+	String str;
 	void run() {
 		while(true)
 		{
@@ -36,28 +35,27 @@ public class C01StudentMenu {
 		System.out.println("9.종료");
 		System.out.println("------------ 학생메뉴 -------------");
 		System.out.print("입력 : ");
-		no = sc.nextInt();
-		switch(no)
+		str = sc.next();
+		switch(str)
 		{
-		case 1:
+		case "1":
 			//C01StudentMenu객체를 만들고
 			C01Student stu = new C01Student();
 			//각정보 입력한 후
 			System.out.print("ID : ");
 			stu.stdId = sc.next();
-			System.out.print("Name : ");
+			System.out.print("이름 : ");
 			stu.stdName = sc.next();
-			System.out.print("Address : ");
+			System.out.print("주소 : ");
 			stu.stdAddr = sc.next();
-			System.out.print("Phone Number : ");
+			System.out.print("전화번호 : ");
 			stu.stdPhone = sc.next();
 			//ArrayList에 추가합니다
 			Stdlist.add(stu);
 			System.out.println("계정 등록 완료!");
 			break;
-		case 2:
+		case "2":
 			//로그인
-			//ArrayList에 해당 stdId가 있는 idx를 CurIdx에 저장
 			sc.nextLine();
 			System.out.print("ID : ");
 		    String id = sc.nextLine();
@@ -71,12 +69,11 @@ public class C01StudentMenu {
 		        } 
 		    }
 			break;
-		case 3:
+		case "3":
 			//로그아웃
-			//CurIdx 에 9999값 넣음
 			CurIdx = null;
 			break;
-		case 4:
+		case "4":
 			//내가 신청한 과목 정보 출력(여러 과목일수 있음)
 			try {
 				System.out.print("신청한 과목 : ");
@@ -90,7 +87,7 @@ public class C01StudentMenu {
 				System.out.println("신청한 과목이 없습니다.");
 			}
 			break;
-		case 5:
+		case "5":
 			//CurIdx에 있는 학생 정보객체에 과목을 입력받아 저장 
 			System.out.print("신청할 과목 수 : ");
 			int count = sc.nextInt();
@@ -101,7 +98,7 @@ public class C01StudentMenu {
 			}
 			System.out.println("수강 신청 완료!");
 			break;
-		case 6:
+		case "6":
 			//신청 취소할 과목을 입력받아
 			//동일한 과목이 있다면 삭제(NULL값 입력)
 			System.out.print("신청 취소할 과목 : ");
@@ -113,32 +110,59 @@ public class C01StudentMenu {
 				}      
 			}
 	        break;
-		case 7:
+		case "7":
 			//학생정보를 수정
-			System.out.println("학생 정보를 수정합니다.(ID제외)");
-			System.out.print("Name : ");
-			CurIdx.stdName = sc.next();
-			System.out.print("Address : ");
-			CurIdx.stdAddr = sc.next();
-			System.out.print("Phone Number : ");
-			CurIdx.stdPhone = sc.next();
-			System.out.println("수정 완료!");
+			System.out.println("학생 정보를 수정합니다.");
+			System.out.print("ID를 입력해주세요 : ");
+			String editID = sc.next();
+			try {
+				if(CurIdx.stdId.equals(editID)) {
+					System.out.println("현재 이름 : "+CurIdx.stdName);
+					System.out.print("바꿀 이름 : ");
+					CurIdx.stdName = sc.next();
+					System.out.println("현재 주소 : "+CurIdx.stdAddr);
+					System.out.print("바꿀 주소 : ");
+					CurIdx.stdAddr = sc.next();
+					System.out.println("현재 전화번호 : "+CurIdx.stdPhone);
+					System.out.print("바꿀 전화번호 : ");
+					CurIdx.stdPhone = sc.next();
+					System.out.println("수정 완료!");
+		        } else {
+			        System.out.println("ID가 틀립니다.");
+		        }
+			} catch (Exception e) {
+		        System.out.println("ID가 틀립니다.");
+			}
 			break;
-		case 8:
+		case "8":
 			//계정삭제
 			//현재 CurIdx 를 리스트에서 제거 
-			
+			System.out.print("ID를 입력해주세요 : ");
+			String delID = sc.next();
+			try {
+				if(CurIdx.stdId.equals(delID)) {
+					System.out.println("정말로 삭제 하시겠습니까?(\"네\"라고 입력)");
+					String confirm = sc.next();
+					if(confirm.equals("네")) {
+						Stdlist.remove(CurIdx);
+						System.out.println("계정 삭제 완료");
+						CurIdx = null;
+					}
+		        } else {
+			        System.out.println("ID가 틀립니다.");
+		        }
+			} catch (Exception e) {
+		        System.out.println("ID가 틀립니다.");
+			}
 			break;
-		case 9:
+		case "9":
 			//종료
-			return ;
-		default :
+			System.out.println("종료합니다");
+			return;
+		default:
 			//잘못입력 다시
+			System.out.println("잘못 입력했습니다. 다시 입력해주세요.");
 		}
-		
-		
-		
 		}	
-		
 	}
 }
